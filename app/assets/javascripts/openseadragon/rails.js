aa4a3c335e18191fdef0a95668eb24aff55466d3
Chris Beer <cabeer@stanford.edu>
2015-03-18 09:28:31 -0700

    Add proper jquery/openseadragon plugin and provide an install generator

--- app/assets/javascripts/openseadragon/rails.js
+++ app/assets/javascripts/openseadragon/rails.js
@@ -1,42 +1,10 @@
-//= require openseadragon
+//= require openseadragon/jquery
 
 (function($) {
-  var __osd_counter = 0;
-  function generateOsdId() {
-    __osd_counter++;
-    
-    return "Openseadragon" + __osd_counter;
+  function initOpenSeadragon() {
+    $('picture[data-openseadragon]').openseadragon();
   }
 
-  function initOpenSeadragon() {
-    $('picture[data-openseadragon]').each(function() {
-      var $picture = $(this);
-      
-      if (typeof $picture.attr('id') === "undefined") {
-        $picture.attr('id', generateOsdId());
-      }
-
-      var collectionOptions = $picture.data('openseadragon');
-      
-      var sources = $picture.find('source[media="openseadragon"]');
-
-      var tilesources = $.map(sources, function(e) {
-        if ($(e).data('openseadragon')) {
-          return $(e).data('openseadragon');
-        } else {
-          return $(e).attr('src');
-        }
-      });
-      
-      $picture.css("display", "block");
-      $picture.css("height", "500px");
-
-      $picture.data('osdViewer', OpenSeadragon(
-        $.extend({ id: $picture.attr('id') }, collectionOptions, { tileSources: tilesources })
-      ));
-    });
-  };
-
-  window.onload = initOpenSeadragon;
-  document.addEventListener("page:load", initOpenSeadragon, false);
+  $(document).on('page:load', initOpenSeadragon);
+  $(document).ready(initOpenSeadragon);
 })(jQuery);
