7254b7ade1f1354d07c8da4cd9f735ab77cf5cd8
Justin Coyne <jcoyne@justincoyne.com>
2016-11-04 10:56:52 -0400

    Turbolinks 5 support.

--- app/assets/javascripts/openseadragon/rails.js
+++ app/assets/javascripts/openseadragon/rails.js
@@ -7,4 +7,16 @@
 
   $(document).on('page:load', initOpenSeadragon);
   $(document).ready(initOpenSeadragon);
+
+  var handler = 'ready';
+  if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+    // Turbolinks 5
+    if (Turbolinks.BrowserAdapter) {
+      handler = 'turbolinks:load';
+    } else {
+      // Turbolinks < 5
+      handler = 'page:load ready';
+    }
+  }
+  $(document).on(handler, initOpenSeadragon);
 })(jQuery);
